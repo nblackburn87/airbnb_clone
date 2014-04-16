fc987653631f4c2490a56719578e84a3ad2c4d03
Maureen Dugan & Natalie Blackburn <student@epicodus.com>
2014-04-16 10:59:02 -0700

    edit strong parameters for devise sign_in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  has_secure_password
-
   validates :name, presence: true
   validates :email, presence: true
 
--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe User do
 
   it 'allows a new user to create an account' do
-    visit new_user_path
+    visit new_user_registration_path
     user = FactoryGirl.build(:user)
     fill_in :user_name, with: user.name
     fill_in :user_password, with: user.password
     fill_in :user_password_confirmation, with: user.password
     fill_in :user_email, with: user.email
-    click_button "Create User"
-    page.should have_content "Your account has been created"
+    click_button "Sign up"
+    page.should have_content "Logged in"
   end
 end
