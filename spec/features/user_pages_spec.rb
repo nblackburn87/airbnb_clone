1415a7939fba5a21b5d6159d246d8fd1e335804a
Maureen Dugan & Natalie Blackburn <student@epicodus.com>
2014-04-16 11:03:52 -0700

    add capybara test for signout error

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe User do
-
   it 'allows a new user to create an account' do
     visit new_user_registration_path
     user = FactoryGirl.build(:user)
@@ -11,5 +10,15 @@ describe User do
     fill_in :user_email, with: user.email
     click_button "Sign up"
     page.should have_content "Logged in"
+  end
+   it 'displays errors if a user enters the wrong information' do
+    visit new_user_registration_path
+    user = FactoryGirl.build(:user)
+    fill_in :user_name, with: user.name
+    fill_in :user_password, with: user.password
+    fill_in :user_password_confirmation, with: "password"
+    fill_in :user_email, with: user.email
+    click_button "Sign up"
+    page.should_not have_content "Logged in"
   end
 end
