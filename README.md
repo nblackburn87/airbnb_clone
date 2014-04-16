11493d2dfbb8cf5f43f29e8245780635bfab7ce0
Maureen Dugan & Natalie Blackburn <student@epicodus.com>
2014-04-16 09:26:36 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+== Open Home (Airbnb Clone)
+
+==== Maureen Dugan & Natalie Blackburn
+
+We are practicing using devise, paperclip, and smarter_csv
+
+* As a user, I want signup and receive a confirmation email, so that I can access the site.
+
+* As a host user, I want to add a listing, so that I can allow others to view and search my home/apt listing.
+
+* As a host user, I want to add photo to my profile, so that other users know I am a person.
+
+* As a host user, I want to add photos of my listing, so that others have a good idea of what my place looks like.
+
+* As a host user, I want to add multiple listings at once by uploading a CSV file, so that I save time.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenHome::Application.config.secret_key_base = '4eb2613b3f89d4def7078029df04b5f6eb313b7386db896293311f15d897c6e5c1017dc9dd94d4171f952bf51cf1ca581ce99c1bf47b12a847f5bd990a6430d5'
