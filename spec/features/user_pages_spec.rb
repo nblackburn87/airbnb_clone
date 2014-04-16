7e41ebdfb6d8f3c65846002e7e9382949fd99dda
Maureen Dugan & Natalie Blackburn <student@epicodus.com>
2014-04-16 13:58:30 -0700

    add tests for edit profile page

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,24 +1,47 @@
 require 'spec_helper'
 
 describe User do
-  it 'allows a new user to create an account' do
-    visit new_user_registration_path
-    user = FactoryGirl.build(:user)
-    fill_in :user_name, with: user.name
-    fill_in :user_password, with: user.password
-    fill_in :user_password_confirmation, with: user.password
-    fill_in :user_email, with: user.email
-    click_button "Sign up"
-    page.should have_content "Logged in"
+  context "creating" do
+    it 'allows a new user to create an account' do
+      visit new_user_registration_path
+      user = FactoryGirl.build(:user)
+      fill_in :user_name, with: user.name
+      fill_in :user_password, with: user.password
+      fill_in :user_password_confirmation, with: user.password
+      fill_in :user_email, with: user.email
+      click_button "Sign up"
+      page.should have_content "Logged in"
+    end
+     it 'displays errors if a user enters the wrong information' do
+      visit new_user_registration_path
+      user = FactoryGirl.build(:user)
+      fill_in :user_name, with: user.name
+      fill_in :user_password, with: user.password
+      fill_in :user_password_confirmation, with: "password"
+      fill_in :user_email, with: user.email
+      click_button "Sign up"
+      page.should_not have_content "Logged in"
+    end
   end
-   it 'displays errors if a user enters the wrong information' do
-    visit new_user_registration_path
-    user = FactoryGirl.build(:user)
-    fill_in :user_name, with: user.name
-    fill_in :user_password, with: user.password
-    fill_in :user_password_confirmation, with: "password"
-    fill_in :user_email, with: user.email
-    click_button "Sign up"
-    page.should_not have_content "Logged in"
+
+  context "editing" do
+    it 'displays the edit page when navigated to' do
+      user = FactoryGirl.create(:user)
+      sign_in(user)
+      click_link "Edit profile"
+      page.should have_content "New Password (leave"
+    end
+
+    it 'lets a signed in user edit their password' do
+      user = FactoryGirl.create(:user)
+      sign_in(user)
+      visit edit_user_registration_path
+      new_password = Faker::Internet.password
+      fill_in :user_password, with: new_password
+      fill_in :user_password_confirmation, with: new_password
+      fill_in :user_current_password, with: user.password
+      click_button "Update"
+      page.should have_content "You updated your account successfully."
+    end
   end
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,7 @@
+def sign_in(user)
+  visit root_url
+  click_link "Login"
+  fill_in :user_email, with: user.email
+  fill_in :user_password, with: user.password
+  click_button "Sign in"
+end
