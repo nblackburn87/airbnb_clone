1fbfae0254ffac16591f435716691201aa96dda4
Maureen Dugan & Natalie Blackburn <student@epicodus.com>
2014-04-16 09:43:41 -0700

    update homepage styling

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    name Faker::Name.name
+    email Faker::Internet.email
+    password Faker::Internet.password
+    password_confirmation { |u| u.password }
+    host false
+  end
+end
