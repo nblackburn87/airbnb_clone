8c589adf33aeba0db59ec45283eeda762a280a7b
Natalie Blackburn & Maureen Dugan <student@epicodus.com>
2014-04-17 14:49:14 -0700

    add static front page and adjust user permissions

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+
+  def home
+    @listings = Listing.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 OpenHome::Application.routes.draw do
 
-  root to: 'users#index'
+  root to: 'pages#home'
+
+  match '/home', { :via => :get, :to => 'pages#home' }
 
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :users, only: [:index, :show]
