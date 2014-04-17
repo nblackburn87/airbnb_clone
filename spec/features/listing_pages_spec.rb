6261f37981675feb8a613410fcb1e905d45b14c2
Maureen Dugan & Natalie Blackburn <student@epicodus.com>
2014-04-16 17:20:03 -0700

    add tests for edit and delete listings, edit not passing

--- spec/features/listing_pages_spec.rb
+++ spec/features/listing_pages_spec.rb
@@ -1,26 +1,42 @@
 require 'spec_helper'
 
 describe Listing do
-
   context 'creating' do
-
     it 'allows a signed in user to add a new listing' do
       user = FactoryGirl.create(:user)
       sign_in(user)
       click_link "Manage your Listings"
       click_link "Add Listing"
       listing = FactoryGirl.build(:entire_place)
-      fill_in :listing_address, with: listing.address
-      fill_in :listing_city, with: listing.city
-      fill_in :listing_state, with: listing.state
-      fill_in :listing_zipcode, with: listing.zipcode
-      fill_in :listing_number_of_people, with: listing.number_of_people
-      fill_in :listing_price_per_night, with: listing.price_per_night
-      select(listing.property_type, from: :listing_property_type )
-      click_button "Create Listing"
+      create_listing(listing)
       page.should have_content "New listing created"
     end
   end
-
-
+  context 'editing' do
+    it 'allows anyone to view the details of a listing' do
+      listing = FactoryGirl.create(:shared_room)
+      visit listing_path(listing)
+      page.should have_content listing.address
+      page.should_not have_content "Edit listing"
+    end
+    it 'allows user who is signed in to edit their own listing' do
+      user = FactoryGirl.create(:user)
+      sign_in(user)
+      listing = FactoryGirl.create(:entire_place)
+      create_listing(listing)
+      visit edit_listing_path(listing)
+      fill_in :listing_address, with: Faker::Address.street_address
+      click_button "Update"
+      page.should have_content "Listing has been updated."
+    end
+    it 'allows a user who is signed in to delete their own listing' do
+      user = FactoryGirl.create(:user)
+      sign_in(user)
+      listing = FactoryGirl.create(:entire_place)
+      create_listing(listing)
+      visit listing_path(listing)
+      click_link "Delete listing"
+      page.should have_content "Your listing has been deleted."
+    end
+  end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -5,3 +5,16 @@ def sign_in(user)
   fill_in :user_password, with: user.password
   click_button "Sign in"
 end
+
+def create_listing(listing)
+  visit new_listing_path
+  fill_in :listing_address, with: listing.address
+  fill_in :listing_city, with: listing.city
+  fill_in :listing_state, with: listing.state
+  fill_in :listing_zipcode, with: listing.zipcode
+  fill_in :listing_number_of_people, with: listing.number_of_people
+  fill_in :listing_price_per_night, with: listing.price_per_night
+  select(listing.property_type, from: :listing_property_type )
+  click_button "Create Listing"
+end
+
