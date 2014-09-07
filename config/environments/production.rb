dccdd1c7f8d1f6381d178024b44ea3dfef398030
Natalie <blackburn87@gmail.com>
2014-09-07 10:57:27 -0700

    add smtp config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,17 @@ OpenHome::Application.configure do
   # Set to :debug to see everything in the log.
   config.log_level = :info
 
+  # Setup action_mailer in production
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'test@gmail.com',
+    password:             'password',
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
@@ -64,8 +75,7 @@ OpenHome::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_deliveries = false
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
